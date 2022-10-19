b6dd2179e0b47e58d34a19b0ff15c22dd789cc7f
Adrián Acosa Sánchez <adri110301@gmail.com>
2022-10-19 17:53:36 +0200

    Arreglar vistas y preparacion en tabla

--- practica-3/p3-2/app/recetas/urls.py
+++ practica-3/p3-2/app/recetas/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('?busqueda=<str:busqueda>', views.vista_tablas, name='vista-tablas'),
 ]
--- practica-3/p3-2/app/recetas/views.py
+++ practica-3/p3-2/app/recetas/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from recetas.models import Receta
 
 # Create your views here.
 
 def index(request):
-    oscuro = ''
-    return render(request, 'recetas/lista_recetas.html', {'titulo': 'Recetas', 'recetas': '', 'oscuro': oscuro})
+    if request.method == 'GET':
+        oscuro = request.GET.get('oscuro')
+    else:
+        oscuro = ''
+    return render(request, 'recetas/base.html', {'titulo': 'Recetas', 'oscuro': oscuro})
+
+def vista_tablas(request):
+    if request.method == 'GET':
+        oscuro = request.GET.get('oscuro')
+    else:
+        oscuro = ''
+    recetas = Receta.objects.all()
+    return render(request, 'recetas/lista_recetas.html', {'titulo': 'Recetas', 'recetas': recetas, 'oscuro': oscuro})
