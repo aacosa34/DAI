c648186deacb472f051ed3ac3394a22021f8d765
Adrián Acosa Sánchez <adri110301@gmail.com>
2022-10-20 19:28:05 +0200

    Sesiones terminadas y ajustado de nuevo CSS

--- practica-3/README.md
+++ practica-3/README.md
@@ -2,6 +2,7 @@
 
 Subdirectorios de las distintas partes de la práctica 3.
 
-## Partes de la práctica 2
+## Partes de la práctica 3
 
 - [Practica 3.1](p3-1): Django
+- [Practica 3.2](p3-2): Uso de plantillas, sesiones y TailwindCSS (Framework de CSS a elegir)
--- practica-3/p3-2/app/recetas/views.py
+++ practica-3/p3-2/app/recetas/views.py
@@ -4,14 +4,28 @@ from recetas.models import Receta
 # Create your views here.
 
 def index(request):
-    request.session['theme'] = 'dark'
+    if request.method == 'POST':
+        request.session['theme'] = request.POST['theme']
+    
+    if 'theme' in request.session:
+        theme = request.session['theme']
+    else:
+        request.session['theme'] = 'light'
 
-    return render(request, 'recetas/base.html', {'titulo': 'Recetas'})
+    return render(request, 'recetas/base.html', {'titulo': 'Recetas', 'theme': theme})
 
 def busqueda(request):
-    if request.method == 'GET' and 'busqueda' in request.GET:
+    if request.method == 'POST':
+        request.session['theme'] = request.POST['theme']
+
+    if 'theme' in request.session:
+        theme = request.session['theme']
+    else:
+        request.session['theme'] = 'light'
+
+    if request.method == 'GET' or 'busqueda' in request.GET:
         recetas = Receta.objects.filter(nombre__startswith=request.GET['busqueda']).values()
     else:
         recetas = {}
 
-    return render(request, 'recetas/lista_recetas.html', {'titulo': 'Recetas', 'recetas': recetas})
+    return render(request, 'recetas/lista_recetas.html', {'titulo': 'Recetas', 'recetas': recetas, 'theme': theme})
